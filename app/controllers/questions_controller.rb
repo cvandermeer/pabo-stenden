60e2b28a572a9b3eb139385685de95a26ffefae7
Bart Hoekstra <bart-hoekstra@live.nl>
2015-03-10 16:30:21 +0100

    Ask Questions is done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,32 @@
+class QuestionsController < ApplicationController
+  
+  layout 'application'
+
+  def index
+    @questions = Question.all
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create 
+    @question = Question.new(question_params);
+    if @question.save
+      redirect_to('/questions')
+    else
+      render('questions/new')
+    end
+  end
+
+  private
+
+    def question_params
+      params.require(:question).permit(:title, :body)
+    end
+
+end
