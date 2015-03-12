c06e3b3f4eab0327f7f7fc72ee3b095ee1f905ff
Chris van der Meer <cvandermeer@live.nl>
2015-03-12 07:51:43 +0100

    Adding authentication and tests

--- test/controllers/answers_controller_test.rb
+++ test/controllers/answers_controller_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class AnswersControllerTest < ActionController::TestCase
 	test "should be able to create answer" do
+		sign_in users(:one)
 		answer = answers(:answer1)
 		assert_difference("Answer.count") do
 			post :create, answer: {body: answer.body, question_id: answer.question}
--- test/controllers/questions_controller_test.rb
+++ test/controllers/questions_controller_test.rb
@@ -2,11 +2,13 @@ require 'test_helper'
 
 class QuestionsControllerTest < ActionController::TestCase
 	test "should get new" do
+		sign_in users(:one)
 		get :new
 		assert_response :success
 	end
 
 	test "should be able to create question" do
+		sign_in users(:one)
 		question = questions(:question1)
 		assert_difference("Question.count") do
 			post :create, question: {title: question.title, body: question.body}
@@ -16,11 +18,13 @@ class QuestionsControllerTest < ActionController::TestCase
 	end
 
 	test "should get index" do
+		sign_in users(:one)
 		get :index
 		assert_response :success
 	end
 
 	test "should get new question link" do
+		sign_in users(:one)
 		question = questions(:question1)
 		get(:show, {id: question})
 		assert_response :success
