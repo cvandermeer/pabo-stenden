abd24595dbe8052983cf2240f27b4f321dbe4b6a
Chris van der Meer <cvandermeer@live.nl>
2015-03-19 11:00:27 +0100

    Some minor fixes and relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable :registerable
   devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :registerable
+  ### RELATIONS ###
+  has_many :questions
 end
