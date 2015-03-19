3186daab23f6ca6d46f93d099601f575478c528f
Chris van der Meer <cvandermeer@live.nl>
2015-03-19 11:36:00 +0100

    Removing password recovery

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable :registerable
   devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable, :registerable
+         :rememberable, :trackable, :validatable, :registerable
   ### RELATIONS ###
   has_many :questions
 end
