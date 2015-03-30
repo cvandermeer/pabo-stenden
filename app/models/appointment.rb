fd258d112e01701f4fb50c901780c709916351a7
Chris van der Meer <cvandermeer@live.nl>
2015-03-30 15:34:47 +0200

    validation fix

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -7,5 +7,5 @@ class Appointment < ActiveRecord::Base
 	validates :date, presence: true
 	validates :start_time, presence: true
 	validates :end_time, presence: true
-	validates :agenda_id
+	validates :agenda_id, presence: true
 end
